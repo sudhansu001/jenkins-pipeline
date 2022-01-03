2cd6b88eca2b5aa83047d969cfb745c44637924a
Sudhansu Shrestha <sudhansu.shrestha@cgifederal.com>
2022-01-03 11:34:29 -0500

    pipeline v1

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,39 @@
 pipeline{
     agent any
     stages{
-        stage("hello"){
+        stage("Initialize Pipeline"){
             steps{
-                echo "Hello World"
+                echo "Initialize"
+            }
+        }
+        stage("Build"){
+            steps{
+                echo "Build code"
+            }
+        }
+        stage("Code Quality"){
+            steps{
+                echo "Run Code quality"
+            }
+        }
+        stage("Artifactory"){
+            steps{
+                echo "Upload Artifact to Artifactory"
+            }
+        }
+        stage("IAC Provision"){
+            steps{
+                echo "Provision Architecture"
+            }
+        }
+        stage("IAC Deploy"){
+            steps{
+                echo "Deploy Container"
+            }
+        }
+        stage("IAC Destroy"){
+            steps{
+                echo "Destroy Container"
             }
         }
     }
