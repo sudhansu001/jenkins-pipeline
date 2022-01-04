82739403afd0b15074d16f86ab51de004a9fd1fb
Sudhansu Shrestha <sudhansu.shrestha@cgifederal.com>
2022-01-04 14:27:36 -0500

    try scmurl

--- vars/build_mvn.groovy
+++ vars/build_mvn.groovy
@@ -1,7 +1,9 @@
 
 def call (Map config = [tool: "mvn", args: ""]){
     if (config.tool == "mvn"){
+        def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
         node {
+            bat "git clone ${scmUrl}"
             bat "mvn clean install ${config.args}"
         }
     }
