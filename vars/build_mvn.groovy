6bf581bf0aaaa9624c549775b40958f3ed5e3986
Sudhansu Shrestha <sudhansu.shrestha@cgifederal.com>
2022-01-04 14:36:54 -0500

    try git clone in same directory

--- vars/build_mvn.groovy
+++ vars/build_mvn.groovy
@@ -3,7 +3,9 @@ def call (Map config = [tool: "mvn", args: ""]){
     if (config.tool == "mvn"){
         def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
         node {
-            bat "git clone ${scmUrl} ."
+            bat "git init
+                 git remote add origin ${scmUrl}
+                 git pull origin master"
             bat "mvn clean install ${config.args}"
         }
     }
